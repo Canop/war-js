37e6a5818a5fd7599e05ae37c416d01161fd916c
Abhishek Hingnikar <djdark88.7@gmail.com>
2012-08-25 20:11:15 +0530

    First Commit

---
+++ js/three.css.js
@@ -0,0 +1,13 @@
+window.three = new (function(){
+	'use strict';
+	var objects = [],
+		lights  = [];
+	this.AddObject =  function( domNode , properties ){};
+	this.AddLight  =  function( data ){};
+	
+	this.loadMap   =  function ( ArrayData ){
+		/* Map is an array Data [
+		 { // Object for a wall  }
+		] */	
+	};
+})();
