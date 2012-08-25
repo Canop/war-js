31707473292c7533787312016a1c386f98353cf0
Abhishek Hingnikar <djdark88.7@gmail.com>
2012-08-26 00:59:17 +0530

    Added Maps 0.001

--- js/three.css.js
+++ js/three.css.js
@@ -1,13 +1,50 @@
-window.three = new (function(){
+window.three = function(id,xWid,yWid,res){
 	'use strict';
-	var objects = [],
-		lights  = [];
+	var world = document.getElementById(id),
+		objects = [],
+		lights  = [],
+		terrain = world.getElementsByClassName('terrian')[0],
+		xWidth  = xWid,
+		yWidth  = yWid,
+		resolution = res; // The resolution of terrain
+		
 	this.AddObject =  function( domNode , properties ){};
 	this.AddLight  =  function( data ){};
 	
-	this.loadMap   =  function ( ArrayData ){
-		/* Map is an array Data [
-		 { // Object for a wall  }
-		] */	
+	this.createMap   =  function ( terrain_type ){
+		var tPatch =  document.createElement('li');
+		tPatch.className += ' tile ';
+		tPatch.className += terrain_type;
+		tPatch.style.height = tPatch.style.width = resolution + 'px';
+		
+		var map_z = []
+		,tempNode,
+		xMax = Math.ceil(xWidth/resolution),
+		yMax = Math.ceil(yWidth/resolution);
+		for( var i = 0; i < 1 ; i++ ){
+			map_z[i] = [];
+			for( var j= 0; j < xMax ; j++ ){
+				tempNode = tPatch.cloneNode();
+				/* okay fixing it only in 1 axis now */
+				var z = ( Math.random()*10 )- 5;
+				map_z[i][j] = z;
+				
+				if( map_z[i][j-1] != undefined && map_z[i][j-1] !== z ){
+					console.log("Z value:",z," Preceders value:",map_z[i][j-1]);
+					var dz = map_z[i][j-1] - z,
+						slop = dz/resolution,
+						ln = Math.sqrt(dz*dz + resolution*resolution),
+						scale = ln/resolution;
+						z += dz/2;
+						var angle = Math.acos(1/scale);
+						console.log( "Delta Z : ",dz,"slope : ",slop,"Assigned Z :",z);
+						tempNode.style.webkitTransform = 'scaleX('+scale+') rotateY( '+ angle +'rad)';	
+				}
+				tempNode.style.left = j*resolution+'px';
+				tempNode.style.top  = i*resolution+'px';
+				tempNode.style.webkitTransform += ' translateZ('+z+'px)';
+				terrain.appendChild(tempNode);
+			}
+		} 
 	};
-})();
+};
